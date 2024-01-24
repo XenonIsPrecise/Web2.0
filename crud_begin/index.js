80c26eba4d2a9a28f5dfdd662350a6ec6a8f3c09
Sujal Acharya <sujalacharya567@gmail.com>
2024-01-23 23:06:08 -0500

    random commit

--- crud_begin/index.js
+++ crud_begin/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { default: mongoose } = require('mongoose');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
+
 
 const Product = require('./models/product');
 
@@ -18,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/khetBari')
 app.set('views', path.join(__dirname,'views'))
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_methid'))
 
 app.get('/products',async(req,res)=>{
     const products = await Product.find({})
